68e86ed5071140af23e6602478194e8f16124ff2
Ryo Yoshida <ryo.yoshida@utexas.edu>
2018-04-04 17:51:31 -0500

    Remove content_main.xml

--- app/src/main/java/ee461l/hw4/MainActivity.java
+++ app/src/main/java/ee461l/hw4/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,7 +11,6 @@ import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
